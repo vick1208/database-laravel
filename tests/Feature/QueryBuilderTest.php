e70513cf91896b2de83b347b57b083dec9435beb
Vicky AS <vickyhuang808@gmail.com>
2023-07-07 07:10:06 +0700

    coba query builder select

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -5,15 +5,18 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertNotNull;
 
 class QueryBuilderTest extends TestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
-        // DB::delete('delete from categories');
-        DB::table('categories')->truncate();
+        DB::delete('delete from categories');
+        // DB::table('categories')->truncate();
     }
 
     public function testInsertRow(): void
@@ -33,6 +36,13 @@ class QueryBuilderTest extends TestCase
 
     public function testSelectRow(): void
     {
-        
+        $this->testInsertRow();
+        $data = DB::table('categories')->select(["id","name"])->get();
+        assertNotNull($data);
+
+        $data->each(function ($item) {
+            Log::info(json_encode($item));
+        });
+
     }
 }
